7110400d7b817ac4ec8bb312ce1f09685398ddd2
Denny <denny.headrick@gmail.com>
2018-04-14 21:09:43 -0500

    update images

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -34,7 +34,7 @@
       <!--chapters -->
       <ListView for="chapter in chapters" v-show="mode==='chapters'">
         <v-template>
-          <Button class="list-button" :text="chapter.name" @tap="onChapterTap(chapter)" />
+          <Button class="list-button" :text="chapter.name + '-' + chapter.title" @tap="onChapterTap(chapter)" />
         </v-template>
       </ListView>
 
@@ -50,101 +50,94 @@
   </Page>
 </template>
 <style>
-
-Image{
-  width:80%;
-  margin:auto;
+Image {
+  width: 80%;
+  margin: auto;
 }
 
-.list-button{
- 
-  border-color:#00529B;
-  color:#00529B;
-  margin-left:4px;
-  margin-right:4px;
-  border-radius:10px;
+.list-button {
+  border-color: #00529b;
+  color: #00529b;
+  margin-left: 4px;
+  margin-right: 4px;
+  border-radius: 10px;
 }
-ScrollView, ListView, WebView{
-  height:100%;
+ScrollView,
+ListView,
+WebView {
+  height: 100%;
 }
 
-#handbook-1-title{
-  font-size:18em;
-  text-align:center;
-  color:#00529B;
+#handbook-1-title {
+  font-size: 18em;
+  text-align: center;
+  color: #00529b;
 }
-.ab{
-  background-color:#00529B;
-  color:white;
+.ab {
+  background-color: #00529b;
+  color: white;
 }
 </style>
 
 
 <script>
-
 import * as webViewModule from "tns-core-modules/ui/web-view";
 import webViewInterfaceModule from "nativescript-webview-interface";
 import appSettings from "tns-core-modules/application-settings";
-
+import chapters from "../scripts/chapters";
 
 export default {
   data() {
     /**/
     return {
-      mode:'home',
-      src:'~/www/afh1.html',
+      mode: "home",
+      src: "~/www/afh1.html",
       isInitialized: false,
-      searchTerm:'',
-      searchResults:[]
+      searchTerm: "",
+      searchResults: []
     };
   },
-  filters:{
-    shorten(text){
-      
-    }
+  filters: {
+    shorten(text) {}
   },
-  computed:{
-    chapters(){
-      return [{name:'Chapter 1', id:'chapter-1'},{name:'Chapter 2', id:'chapter-2'},{name:'Chapter 3', id:'chapter-3'},{name:'Chapter 4', id:'chapter-4'},{name:'Chapter 5', id:'chapter-5'},{name:'Chapter 6', id:'chapter-6'},{name:'Chapter 7', id:'chapter-7'},{name:'Chapter 8', id:'chapter-8'},{name:'Chapter 9', id:'chapter-9'},{name:'Chapter 10', id:'chapter-10'},{name:'Chapter 11', id:'chapter-11'},{name:'Chapter 12', id:'chapter-12'},{name:'Chapter 13', id:'chapter-13'},{name:'Chapter 14', id:'chapter-14'},{name:'Chapter 15', id:'chapter-15'},{name:'Chapter 16', id:'chapter-16'},{name:'Chapter 17', id:'chapter-17'},{name:'Chapter 18', id:'chapter-18'},{name:'Chapter 19', id:'chapter-19'},{name:'Chapter 20', id:'chapter-20'},{name:'Chapter 21', id:'chapter-21'},{name:'Chapter 22', id:'chapter-22'},{name:'Chapter 23', id:'chapter-23'},{name:'Chapter 24', id:'chapter-24'},{name:'Chapter 25', id:'chapter-25'}];
+  computed: {
+    chapters() {
+      return chapters;
     },
-    routes(){
+    routes() {
       return [
-        {name:'Chapters', mode:'chapters'},
-        {name:'Continue Reading', mode:'reader'},
-        {name:'Search', mode:'search'}
-      ]
+        { name: "Chapters", mode: "chapters" },
+        { name: "Continue Reading", mode: "reader" },
+        { name: "Search", mode: "search" }
+      ];
     }
   },
-  watch:{
-    searchTerm(val){
-      if(this.oWebViewInterface){
-        this.oWebViewInterface.emit('twSearch', val);
-        
+  watch: {
+    searchTerm(val) {
+      if (this.oWebViewInterface) {
+        this.oWebViewInterface.emit("twSearch", val);
       }
-
     }
   },
 
   methods: {
-    shorten(text){
-      return text.length > 140 ?
-             text.substr(0, 140) + '...' :
-             text;
+    shorten(text) {
+      return text.length > 140 ? text.substr(0, 140) + "..." : text;
     },
-    onChapterTap(chapter){
+    onChapterTap(chapter) {
       this.updateHash(chapter.id);
-      this.mode = 'reader';
+      this.mode = "reader";
     },
-    onSearch(results){
-      this.$set(this, 'searchResults', results);
+    onSearch(results) {
+      this.$set(this, "searchResults", results);
     },
 
-    updateHash(hash){
-      if(this.oWebViewInterface){
-        this.oWebViewInterface.emit('twUpdateHash',hash);
+    updateHash(hash) {
+      if (this.oWebViewInterface) {
+        this.oWebViewInterface.emit("twUpdateHash", hash);
       } else {
-        this.src = '~/www/afh1.html#' + hash;
-      } 
+        this.src = "~/www/afh1.html#" + hash;
+      }
     },
 
     onWebViewLoad() {
@@ -159,18 +152,18 @@ export default {
         android.getSettings().setAllowFileAccess(true);
         android.getSettings().setAllowFileAccessFromFileURLs(true);
         android.getSettings().setDisplayZoomControls(false);
-     
       }
-      
-        this.oWebViewInterface = new webViewInterfaceModule.WebViewInterface(
-          nativeView,
-          this.src
-        );
-        this.oWebViewInterface.on('tnCreateBookmark', this.onCreateBookmark);
-        this.oWebViewInterface.on('tnSearch', this.onSearch)
-        this.oWebViewInterface.on('tnDebug', function(text){ alert(text); });
+
+      this.oWebViewInterface = new webViewInterfaceModule.WebViewInterface(
+        nativeView,
+        this.src
+      );
+      this.oWebViewInterface.on("tnCreateBookmark", this.onCreateBookmark);
+      this.oWebViewInterface.on("tnSearch", this.onSearch);
+      this.oWebViewInterface.on("tnDebug", function(text) {
+        alert(text);
+      });
     }
-    
   }
 };
 </script>
---
+++ src/scripts/chapters.js
@@ -0,0 +1,52 @@
+export default [
+  { name: "Chapter 1", title: "Air Force Heritage", id: "chapter-1" },
+  { name: "Chapter 2", title: "Enlisted History", id: "chapter-2" },
+  { name: "Chapter 3", title: "Organization", id: "chapter-3" },
+  {
+    name: "Chapter 4",
+    title: "Air Force Doctrine, Air Expeditionary (AEF) and Joint Force",
+    id: "chapter-4"
+  },
+  { name: "Chapter 5", title: "Emergency Management", id: "chapter-5" },
+  { name: "Chapter 6", title: "Standards of Conduct", id: "chapter-6" },
+  {
+    name: "Chapter 7",
+    title: "Enforcing Standards and Legal Issues",
+    id: "chapter-7"
+  },
+  {
+    name: "Chapter 8",
+    title: "Military Customs, Courtesies, and Protocol for Special Events",
+    id: "chapter-8"
+  },
+  { name: "Chapter 9", title: "The Noncommissioned Officer", id: "chapter-9" },
+  { name: "Chapter 10", title: "Leadership", id: "chapter-10" },
+  {
+    name: "Chapter 11",
+    title:
+      "Officer and Enlisted Evaluation Systems and Civilian Performance Program",
+    id: "chapter-11"
+  },
+  { name: "Chapter 12", title: "Training and Education", id: "chapter-12" },
+  { name: "Chapter 13", title: "Resource Management", id: "chapter-13" },
+  {
+    name: "Chapter 14",
+    title: "Communicating in Today's Air Force",
+    id: "chapter-14"
+  },
+  { name: "Chapter 15", title: "Personnel Programs", id: "chapter-15" },
+  { name: "Chapter 16", title: "Wing Support", id: "chapter-16" },
+  { name: "Chapter 17", title: "Dress and Appearance", id: "chapter-17" },
+  { name: "Chapter 18", title: "Fit Force", id: "chapter-18" },
+  { name: "Chapter 19", title: "Security", id: "chapter-19" },
+  { name: "Chapter 20", title: "Air Force Knowledge", id: "chapter-20" },
+  { name: "Chapter 21", title: "Organizational Management", id: "chapter-21" },
+  { name: "Chapter 22", title: "Human Resource Development", id: "chapter-22" },
+  {
+    name: "Chapter 23",
+    title: "Critical Thinking and Decision-Making",
+    id: "chapter-23"
+  },
+  { name: "Chapter 24", title: "Studying Effectively", id: "chapter-24" },
+  { name: "Chapter 25", title: "Professionalism", id: "chapter-25" }
+];
